a040d9751b4ec68afdf882cdc93aab7d510a2d44
Roman Pishchalnikov <roman.pishchalnikov@gmail.com>
2020-04-26 21:01:40 +0700

    Add cd command

--- exec.go
+++ exec.go
@@ -10,6 +10,10 @@ func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 	args := strings.Split(input, " ")
 
+	if args[0] == "cd" {
+		return os.Chdir(args[1])
+	}
+
 	cmd := exec.Command(args[0], args[1:]...)
 
 	cmd.Stdout = os.Stdout
