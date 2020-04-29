6776857a7adf8ea7fa6e1fdc8ff9b4c05776fe7f
Roman Pishchalnikov <roman.pishchalnikov@gmail.com>
2020-04-29 22:54:54 +0700

    Add exit command

--- exec.go
+++ exec.go
@@ -10,11 +10,14 @@ func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 	args := strings.Split(input, " ")
 
-	if args[0] == "cd" {
+	switch args[0] {
+	case "cd":
 		if len(args) < 2 {
 			return os.Chdir(os.Getenv("HOME"))
 		}
 		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
 	}
 
 	cmd := exec.Command(args[0], args[1:]...)
